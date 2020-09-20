871dbeda345502e904ec970de421c5fcb487679e
Noberto Pessoa <no.neto@live.com>
2020-09-20 16:36:24 -0300

    Primeiro commit

---
+++ src/main/java/com/bti/PDSapp/PdSappApplication.java
@@ -0,0 +1,13 @@
+package com.bti.PDSapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdSappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PdSappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bti/PDSapp/PdSappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bti.PDSapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PdSappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
